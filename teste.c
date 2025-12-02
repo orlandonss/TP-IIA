88ead42f488995e5345a2e2d47f8be3d4c589ebf
Luiz Antonio Rocha Leal Dantas Diniz <luizantoniodinizrocha@gmail.com>
2025-12-02 12:21:50 +0000

    teste de commit IIA

--- teste.c
+++ teste.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-printf("isto e um teste o seu fdp!!");
-return 0;
+int main()
+{
+    printf("isto e um teste o seu fdp!!");
+    int i, b;
+    for (i = 0; i < 10; i++)
+    {
+        b = i * 2;
+        printf("\n%d", b);
+    }
+    return 0;
 }
