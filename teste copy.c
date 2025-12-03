669aff9b78789b1639341b3a70dc5b571c795e98
Luiz Antonio Rocha Leal Dantas Diniz <luizantoniodinizrocha@gmail.com>
2025-12-03 18:01:06 +0000

    ficha 7 da aula

---
+++ teste copy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("isto e um teste o seu fdp!!");
+    int i, b;
+    for (i = 0; i < 10; i++)
+    {
+        b = i * 2;
+        printf("\n%d", b);
+    }
+    return 0;
+}
